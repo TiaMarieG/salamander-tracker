422c23fc2aab01501daf3ba27ab1c8711f820bf6
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-06-01 17:00:47 -0700

    Initialized project and set up a home page and test component

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+//Creating a home page 
+
+export default function Home() {
+  return (
+    <div>
+      Welcome to the Home Page!
+    </div>
+  );
+}
---
+++ src/components/Test.jsx
@@ -0,0 +1,9 @@
+// Test component purely to see that it works in the app folders
+
+export default function Test() {
+   return(
+      <div>
+         This was imported from a component
+      </div>
+   )
+}
